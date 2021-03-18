de7023bac21a684ce172ce7dc2131745fab42b0f
Louise Ballantyne <lousballantyne@icloud.com>
2021-03-18 12:04:37 +0000

    Finished up to Player 2 Attack Confirmation

--- app.rb
+++ app.rb
@@ -11,12 +11,24 @@ enable :sessions
   post '/names' do
     session[:player1] = params[:player1]
     session[:player2] = params[:player2]
+    session[:player_2_HP] = 100
     redirect '/play'
   end
+
+  post '/attack_player_2' do
+    session[:player_2_HP] -= 1
+    session[:player_2_attacked] = true
+    session[:player_1_attacked] = false
+    redirect '/play'
+  end
+
 
   get '/play' do
     @player1 = session[:player1]
     @player2 = session[:player2]
+    @player_2_HP = session[:player_2_HP]
+    @player_1_attacked = session[:player_1_attacked]
+    @player_2_attacked = session[:player_2_attacked]
     erb(:play)
   end
 
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -3,10 +3,7 @@ require './app'
 describe Battle do
   describe "The Homepage", type: :feature do
     before do
-      visit '/'
-      fill_in 'player1', with: 'Caesar'
-      fill_in 'player2', with: 'Pixie'
-      click_button 'submit'
+      sign_in_and_play
     end
     it "Shows the entered names" do
       expect(page).to have_text 'Caesar'
@@ -14,6 +11,12 @@ describe Battle do
     end
     it "Shows Player 2's Hit Points" do
       expect(page).to have_text "Pixie's Hit Points:"
+    end
+    context "Players can attack each other" do
+      it "Player 1 can attack Player 2" do
+        click_on('attack_player_2')
+        expect(page).to have_text "Player 2 has been attacked!"
+      end
     end
   end
 end
